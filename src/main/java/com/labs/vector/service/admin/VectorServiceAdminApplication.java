2f69247b3a4d29b455cae61c17d847816d2df9a3
Tanmay <tanmay.chaudhari@vectorlab.in>
2025-01-06 02:47:55 +0530

    vector-admin-portal-service

---
+++ src/main/java/com/labs/vector/service/admin/VectorServiceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.labs.vector.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorServiceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorServiceAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labs/vector/service/admin/VectorServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.vector.service.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
