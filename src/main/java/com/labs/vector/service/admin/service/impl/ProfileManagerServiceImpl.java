0b82b85266724793462626f7da449568ad0014b9
Tanmay Chaudhari <trainee@oaklandsystems.net>
2025-02-13 23:32:11 +0530

    Vector-admin Profile manager apis

---
+++ src/main/java/com/labs/vector/service/admin/service/impl/ProfileManagerServiceImpl.java
@@ -0,0 +1,34 @@
+package com.labs.vector.service.admin.service.impl;
+
+import com.labs.vector.service.admin.model.VectorRegisteredUser;
+import com.labs.vector.service.admin.repository.VectorRegisteredUserRepository;
+import com.labs.vector.service.admin.service.ProfileManagerService;
+import com.labs.vector.service.admin.utils.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileManagerServiceImpl implements ProfileManagerService {
+
+    @Autowired
+    VectorRegisteredUserRepository vectorRegisteredUserRepository;
+
+    @Override
+    public ResponseEntity<?> getProfileDetails(Integer vectorUserID) {
+        try {
+            if(vectorUserID != null && vectorUserID > 0) {
+                VectorRegisteredUser vectorUser = vectorRegisteredUserRepository.findById(vectorUserID)
+                        .orElseThrow(() ->
+                                new UserNotFoundException(
+                                        "User not found for userID: " + vectorUserID, "404"
+                                ));
+                return ResponseEntity.ok(vectorUser);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
